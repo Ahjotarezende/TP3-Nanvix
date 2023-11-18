fd0b71687dc1bfcb852a0938c958cac6324de0ee
Alexandre <alexandmagara@gmail.com>
2023-11-18 11:11:28 -0300

    Adicionando semctl

--- include/nanvix/syscall.h
+++ include/nanvix/syscall.h
@@ -269,6 +269,8 @@ EXTERN int sys_get_process_info(pid_t pid, struct process_buf *buf);
 
 EXTERN int sys_semget(unsigned key);
 
+EXTERN int sys_semctl(int semid, int cmd, int val);
+
 EXTERN int sys_semop(int id, int op);
 
 #endif /* _ASM_FILE_ */
